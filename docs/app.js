600f8b6cd7fa090563b82eb1b018b72fd6b74ef7
Julius Kiekbusch <jubeki99@gmail.com>
2021-01-02 00:04:11 +0100

    Add lessons and contents to app.js

--- docs/app.js
+++ docs/app.js
@@ -29,10 +29,76 @@ courselists.forEach(courselist => {
     
             var child = template.content.cloneNode(true)
     
-            child.querySelector("[data-course='illustration']").src = course.getAttribute("illustration")
-            child.querySelector("[data-course='title']").innerHTML = course.getElementsByTagName("title")[0].innerHTML
-            child.querySelector("[data-course='description']").innerHTML = course.getElementsByTagName("description")[0].innerHTML
+            var illustration = child.querySelector("[data-course='illustration']")
+            var data = course.getAttribute("illustration")
+            if(illustration) {
+                if(data) {
+                    illustration.src = data
+                } else {
+                    illustration.classList.add('hidden')
+                }
+            }
     
+            var title = child.querySelector("[data-course='title']")
+            data = course.getElementsByTagName("title")[0].innerHTML
+            if(title) {
+                if(data) {
+                    title.innerHTML = data
+                } else {
+                    title.classList.add('hidden')
+                }
+            }
+    
+            var description = child.querySelector("[data-course='description']")
+            data = course.getElementsByTagName("description")[0].innerHTML
+            if(description) {
+                if(data) {
+                    description.innerHTML = data
+                } else {
+                    description.classList.add('hidden')
+                }
+            }
+    
+            var lessons = child.querySelector("[data-course='lessons']")
+            data = course.getElementsByTagName("lessons")[0]
+            if(lessons) {
+                if(data) {
+                    var lessonTemplate = lessons.firstElementChild
+                    lessons.innerHTML = ""
+
+                    Array.from(data.children).forEach(lesson => {
+
+                        var li = lessonTemplate.cloneNode(true);
+                        li.innerHTML = lesson.innerHTML
+                        lessons.appendChild(li)
+
+                    })
+
+                } else {
+                    lessons.classList.add('hidden')
+                }
+            }
+    
+            var contents = child.querySelector("[data-course='contents']")
+            data = course.getElementsByTagName("contents")[0]
+            if(contents) {
+                if(data) {
+
+                    var contentTemplate = contents.firstElementChild
+                    contents.innerHTML = ""
+
+                    Array.from(data.children).forEach(content => {
+
+                        var li = contentTemplate.cloneNode(true);
+                        li.innerHTML = content.innerHTML
+                        contents.appendChild(li)
+
+                    })
+                } else {
+                    contents.classList.add('hidden')
+                }
+            }
+
             courselist.appendChild(child);
         })
     
