22383b06942212997fe3ad123ac7c9643ce4996d
Fabian Mildner <mildnfab@b-tu.de>
2021-01-02 13:23:09 +0100

    Add different headers with link activity for legal pages

--- build.js
+++ build.js
@@ -5,6 +5,7 @@ const { JSDOM } = jsdom;
 
 fs.readdir(__dirname + "/src", assembleFiles);
 
+LEGAL_ACTIVITY_DATA_NAMES = ["terms-active", "privacy-active", "imprint-active"];
 function assembleFiles(err, fileList) {
     if (err) {
         return console.error(err);
@@ -23,11 +24,37 @@ function assembleFiles(err, fileList) {
         
             var partialFramework = fs.readFileSync("src/partials/" + partialName + ".html").toString();
         
+            termsPrivacyImprintActive = [false, false, false];
             Array.from(partial.children).forEach(child => {
                 // For each child of the original document's partial marker, look for parameters. If found, replace them into
                 // this partialFramework
-                partialFramework = partialFramework.replace("%" + child.getAttribute("data-name") + "%", child.innerHTML)
+                if (partialName == "header-legal" && LEGAL_ACTIVITY_DATA_NAMES.indexOf(child.getAttribute("data-name")) != -1) {
+                    switch (child.getAttribute("data-name")) {
+                        case "terms-active":
+                            termsPrivacyImprintActive[0] = true;
+                            break;
+                        case "privacy-active":
+                            termsPrivacyImprintActive[1] = true;
+                            break;
+                        case "imprint-active":
+                            termsPrivacyImprintActive[2] = true;
+                            break;
+                        default:
+                    }
+                } else {
+                    partialFramework = partialFramework.replace("%" + child.getAttribute("data-name") + "%", child.innerHTML)
+                }
             })
+            
+            // Set legal link activity given saved parameters (extra loop necessary to clear classlist of non-active links)
+            for (var i in termsPrivacyImprintActive) {
+                var dataName = LEGAL_ACTIVITY_DATA_NAMES[i]
+                if (termsPrivacyImprintActive[i]) {
+                    partialFramework = partialFramework.replace("%" + dataName + "%", "active")
+                } else {
+                    partialFramework = partialFramework.replace("%" + dataName + "%", "")
+                }
+            }
         
             // Replace partial marker with filled-out partialFramework
             partial.outerHTML = partialFramework;
