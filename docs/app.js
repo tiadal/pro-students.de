75a024b08d7f2fd74f6547437c617bf6c5693f65
Fabian Mildner <mildnfab@b-tu.de>
2021-01-02 02:34:36 +0100

    Also add a heading to contents and lessons of courses

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,6 @@
-
+/*
+ * Code for responsive navbar Burger-Menu on mobile
+ */
 const navbar = document.querySelector(".navbar")
 const navbarButton = document.querySelector(".navbar .navbar-left button")
 
@@ -8,9 +10,12 @@ if (navbar != null && navbarButton != null) {
     }
 }
 
-const xmlParser = new DOMParser;
 
-var courselists = document.querySelectorAll("[data-courses]"); // Find elements that are partial markers
+/*
+ * XML Parsing for Course List Pages
+ */
+const xmlParser = new DOMParser;
+var courselists = document.querySelectorAll("[data-courses]"); // Find elements that are a course list (defined by having a data-courses attribute referring to the corresponding XML file)
 courselists.forEach(courselist => {
 
     var xmlFile = courselist.getAttribute("data-courses");
@@ -19,18 +24,22 @@ courselists.forEach(courselist => {
     .then(function (response) {
         return response.text();
     }).then(function (xmlString) {
+        /*
+         * Process the XML file and integrate contents into our HTML document
+         */
 
         const xmlDoc = xmlParser.parseFromString(xmlString, "text/xml")
     
-        var template = courselist.firstElementChild;
+        var template = courselist.firstElementChild; // By definition: element representing a course list should have a template element defining the general structure (only needs to be filled out with concrete information from XML file)
         courselist.innerHTML = "";
     
         Array.from(xmlDoc.documentElement.children).forEach(course => {
+            // Cast each course given in the XML document into an HTML representation and append to HTML course list (courselist)
     
-            var child = template.content.cloneNode(true)
+            var htmlCourse = template.content.cloneNode(true)
     
-            var illustration = child.querySelector("[data-course='illustration']")
-            var data = course.getAttribute("illustration")
+            var illustration = htmlCourse.querySelector("[data-course='illustration']") // Get the element which needs to be filled with content (here, an attribute will have to be set)
+            var data = course.getAttribute("illustration")  // Get the data to fill in from the XML
             if(illustration) {
                 if(data) {
                     illustration.src = data
@@ -39,7 +48,7 @@ courselists.forEach(courselist => {
                 }
             }
     
-            var title = child.querySelector("[data-course='title']")
+            var title = htmlCourse.querySelector("[data-course='title']") // Similar to above; here innerhtml content will be set
             data = course.getElementsByTagName("title")[0].innerHTML
             if(title) {
                 if(data) {
@@ -49,7 +58,7 @@ courselists.forEach(courselist => {
                 }
             }
     
-            var description = child.querySelector("[data-course='description']")
+            var description = htmlCourse.querySelector("[data-course='description']")
             data = course.getElementsByTagName("description")[0].innerHTML
             if(description) {
                 if(data) {
@@ -59,11 +68,11 @@ courselists.forEach(courselist => {
                 }
             }
     
-            var lessons = child.querySelector("[data-course='lessons']")
+            var lessons = htmlCourse.querySelector("[data-course='lessons']")
             data = course.getElementsByTagName("lessons")[0]
             if(lessons) {
                 if(data) {
-                    var lessonTemplate = lessons.firstElementChild
+                    var lessonTemplate = lessons.firstElementChild // Similar to initial approach with courses: we have a "template" list element that can be reproduced to insert actual content
                     lessons.innerHTML = ""
 
                     Array.from(data.children).forEach(lesson => {
@@ -75,11 +84,13 @@ courselists.forEach(courselist => {
                     })
 
                 } else {
+                    console.log(htmlCourse)
+                    htmlCourse.querySelector("h4.lessons-heading").classList.add('hidden')
                     lessons.classList.add('hidden')
                 }
             }
     
-            var contents = child.querySelector("[data-course='contents']")
+            var contents = htmlCourse.querySelector("[data-course='contents']")
             data = course.getElementsByTagName("contents")[0]
             if(contents) {
                 if(data) {
@@ -95,11 +106,12 @@ courselists.forEach(courselist => {
 
                     })
                 } else {
+                    htmlCourse.querySelector("h4.contents-heading").classList.add('hidden')
                     contents.classList.add('hidden')
                 }
             }
 
-            courselist.appendChild(child);
+            courselist.appendChild(htmlCourse);
         })
     
     }).catch(function (err) {
