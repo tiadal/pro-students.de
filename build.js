5ffd419ed90ffc0f398b5d292ce2ad16b71e1a31
Julius Kiekbusch <jubeki99@gmail.com>
2021-01-31 14:11:23 +0100

    Build all Course Previews with annotations

--- build.js
+++ build.js
@@ -12,58 +12,15 @@ function assembleFiles(err, fileList) {
         return console.error(err);
     }
     
+    
     fileList.forEach(htmlDoc => {
         if (!htmlDoc.endsWith('.html')) {
             return;
         }
 
         var dom = new JSDOM(fs.readFileSync(htmlDoc).toString());
-        
-        var partials = dom.window.document.querySelectorAll("[data-partial]"); // Find elements that are partial markers
-        partials.forEach(partial => {
-            var partialName = partial.getAttribute("data-partial");
-        
-            var partialFramework = fs.readFileSync("src/partials/" + partialName + ".html").toString();
 
-            termsPrivacyImprintActive = [false, false, false];
-            Array.from(partial.children).forEach(child => {
-                // For each child of the original document's partial marker, look for parameters. If found, replace them into
-                // this partialFramework
-                if (partialName == "header-legal" && LEGAL_ACTIVITY_DATA_NAMES.indexOf(child.getAttribute("data-name")) != -1) {
-                    switch (child.getAttribute("data-name")) {
-                        case "terms-active":
-                            termsPrivacyImprintActive[0] = true;
-                            break;
-                        case "privacy-active":
-                            termsPrivacyImprintActive[1] = true;
-                            break;
-                        case "imprint-active":
-                            termsPrivacyImprintActive[2] = true;
-                            break;
-                        default:
-                    }
-                } else {
-                    partialFramework = partialFramework.replace("%" + child.getAttribute("data-name") + "%", child.innerHTML)
-                }
-            })
-            
-            // Set legal link activity given saved parameters (extra loop necessary to clear classlist of non-active links)
-            for (var i in termsPrivacyImprintActive) {
-                var dataName = LEGAL_ACTIVITY_DATA_NAMES[i]
-                if (termsPrivacyImprintActive[i]) {
-                    partialFramework = partialFramework.replace("%" + dataName + "%", "active")
-                } else {
-                    partialFramework = partialFramework.replace("%" + dataName + "%", "")
-                }
-            }
-        
-            // Replace partial marker with filled-out partialFramework
-            partial.outerHTML = partialFramework;
-        })
-
-        var ldjsonCourses = assembleXml(dom);
-        
-        var out = dom.serialize().replace("<body></body>", "").replace('%ldjson-courses%', ldjsonCourses);
+        var out = pipeHtml(dom, htmlDoc)
 
         var htmlDocOut = htmlDoc.replace("src/pages/", "docs/")
         var htmlDocOutPath = htmlDocOut.substring(0, htmlDocOut.lastIndexOf('/'))
@@ -77,7 +34,56 @@ function assembleFiles(err, fileList) {
     })
 }
 
-function assembleXml(dom) {
+function pipeHtml(dom, file) {
+    
+    var partials = dom.window.document.querySelectorAll("[data-partial]"); // Find elements that are partial markers
+    partials.forEach(partial => {
+        var partialName = partial.getAttribute("data-partial");
+    
+        var partialFramework = fs.readFileSync("src/partials/" + partialName + ".html").toString();
+
+        termsPrivacyImprintActive = [false, false, false];
+        Array.from(partial.children).forEach(child => {
+            // For each child of the original document's partial marker, look for parameters. If found, replace them into
+            // this partialFramework
+            if (partialName == "header-legal" && LEGAL_ACTIVITY_DATA_NAMES.indexOf(child.getAttribute("data-name")) != -1) {
+                switch (child.getAttribute("data-name")) {
+                    case "terms-active":
+                        termsPrivacyImprintActive[0] = true;
+                        break;
+                    case "privacy-active":
+                        termsPrivacyImprintActive[1] = true;
+                        break;
+                    case "imprint-active":
+                        termsPrivacyImprintActive[2] = true;
+                        break;
+                    default:
+                }
+            } else {
+                partialFramework = partialFramework.replace("%" + child.getAttribute("data-name") + "%", child.innerHTML)
+            }
+        })
+        
+        // Set legal link activity given saved parameters (extra loop necessary to clear classlist of non-active links)
+        for (var i in termsPrivacyImprintActive) {
+            var dataName = LEGAL_ACTIVITY_DATA_NAMES[i]
+            if (termsPrivacyImprintActive[i]) {
+                partialFramework = partialFramework.replace("%" + dataName + "%", "active")
+            } else {
+                partialFramework = partialFramework.replace("%" + dataName + "%", "")
+            }
+        }
+    
+        // Replace partial marker with filled-out partialFramework
+        partial.outerHTML = partialFramework;
+    })
+
+    var ldjsonCourses = assembleXml(dom, file);
+    
+    return dom.serialize().replace("<body></body>", "").replace('%ldjson-courses%', ldjsonCourses);
+}
+
+function assembleXml(dom, file) {
     /*
      * XML Parsing for Course List Pages
      */
@@ -85,7 +91,8 @@ function assembleXml(dom) {
     const xmlParser = new DOMParser
     var courselists = dom.window.document.querySelectorAll("[data-courses]"); // Find elements that are a course list (defined by having a data-courses attribute referring to the corresponding XML file)
 
-    var ldjsonCourses = "";
+    var ldjsonCourses = '{"@context":"http://schema.org","@type":"ItemList","itemListElement":['
+
     courselists.forEach(courselist => {
     
         var xmlFile = courselist.getAttribute("data-courses");
@@ -96,13 +103,21 @@ function assembleXml(dom) {
         var template = courselist.firstElementChild;
         courselist.innerHTML = "";
 
+        var pos = 1;
+
         Array.from(xmlDoc.documentElement.children).forEach(course => {// Cast each course given in the XML document into an HTML representation and append to HTML course list (courselist)
     
             var htmlCourse = template.content.cloneNode(true)
             var ldjsonCourse = '{"@context":"http://schema.org","@type":"Course","name":"%title%","description":"%description%","provider":{"@type":"Organization","name":"Pro Students Cottbus","sameAs":"https://pro-students.kiekbjul.de"}},\n'
+
+    
+
+            var previewName = file.substring(file.lastIndexOf('/') + 1)
+            var previewHtml = fs.readFileSync("src/partials/preview-" + previewName).toString();
     
             var illustration = htmlCourse.querySelector("[data-course='illustration']") // Get the element which needs to be filled with content (here, an attribute will have to be set)
             var data = course.getAttribute("illustration")  // Get the data to fill in from the XML
+            previewHtml = previewHtml.replaceAll('%illustration%', data)
             if(illustration) {
                 if(data) {
                     illustration.src = "/" + data
@@ -113,7 +128,14 @@ function assembleXml(dom) {
     
             var title = htmlCourse.querySelector("[data-course='title']") // Similar to above; here innerhtml content will be set
             data = course.getElementsByTagName("title")[0].innerHTML
+            previewHtml = previewHtml.replaceAll('%title%', data)
+            previewHtml = previewHtml.replaceAll('%title-kebab%', toKebabCase(data))
+            var htmlDocOut = file.replace("src/pages/", "docs/")
+            htmlDocOut = htmlDocOut.substring(0, htmlDocOut.lastIndexOf('.html')) + "/" + toKebabCase(data) + ".html"
+            var htmlDocOutPath = htmlDocOut.substring(0, htmlDocOut.lastIndexOf('/'))
             ldjsonCourse = ldjsonCourse.replace('%title%', data)
+            var ldjsonCourseItem = '{"@type":"ListItem","position":' + pos + ',"url":"' + htmlDocOut.replace("docs/", '/') + '"}'
+            pos++;
             if(title) {
                 if(data) {
                     title.innerHTML = data
@@ -121,10 +143,17 @@ function assembleXml(dom) {
                     title.classList.add('hidden')
                 }
             }
+
+
+            var previewAction = htmlCourse.querySelector("[data-course='preview']")
+            if(previewAction) {
+                previewAction.href = htmlDocOut.replace("docs/", '/');
+            }
     
             var description = htmlCourse.querySelector("[data-course='description']")
             data = course.getElementsByTagName("description")[0].innerHTML
             ldjsonCourse = ldjsonCourse.replace('%description%', data)
+            previewHtml = previewHtml.replaceAll('%description%', data)
             if(description) {
                 if(data) {
                     description.innerHTML = data
@@ -148,9 +177,12 @@ function assembleXml(dom) {
 
                     })
 
+                    previewHtml = previewHtml.replace('%lessons%', lessons.parentElement.outerHTML)
+
                 } else {
                     htmlCourse.querySelector("h3.lessons-heading").classList.add('hidden')
                     lessons.classList.add('hidden')
+                    previewHtml = previewHtml.replace('%lessons%', '')
                 }
             }
     
@@ -169,16 +201,38 @@ function assembleXml(dom) {
                         contents.appendChild(li)
 
                     })
+
+                    previewHtml = previewHtml.replace('%contents%', contents.parentElement.outerHTML)
                 } else {
                     htmlCourse.querySelector("h3.contents-heading").classList.add('hidden')
                     contents.classList.add('hidden')
+                    previewHtml = previewHtml.replace('%contents%', '')
                 }
             }
 
             courselist.appendChild(htmlCourse);
-            ldjsonCourses += ldjsonCourse
+            previewHtml = previewHtml.replace('%ldjson-course%', ldjsonCourse)
+            ldjsonCourses += ldjsonCourseItem
+
+            var previewDom = new JSDOM(previewHtml);
+
+            var previewOut = pipeHtml(previewDom, '')
+
+            try {
+                fs.mkdirSync(htmlDocOutPath)
+            } catch(ex) {}
+    
+            // Save the assembled file
+            fs.writeFile(htmlDocOut, previewOut, err => { if (err) { console.log("Couldn't write " + htmlDocOut + " because of " + err) } })
+
         })
     })
 
-    return ldjsonCourses;
+    return ldjsonCourses + "]}";
+}
+
+function toKebabCase(str) {
+    return str.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+        .map(x => x.toLowerCase())
+        .join('-')
 }
