c9bec5522ca35461ea6537185f6ebca8436d4684
Julius Kiekbusch <jubeki99@gmail.com>
2021-01-31 12:31:57 +0100

    Add Course List to ldjson

--- build.js
+++ build.js
@@ -61,9 +61,9 @@ function assembleFiles(err, fileList) {
             partial.outerHTML = partialFramework;
         })
 
-        assembleXml(dom);
+        var ldjsonCourses = assembleXml(dom);
         
-        var out = dom.serialize().replace("<body></body>", "");
+        var out = dom.serialize().replace("<body></body>", "").replace('%ldjson-courses%', ldjsonCourses);
 
         var htmlDocOut = htmlDoc.replace("src/pages/", "docs/")
         var htmlDocOutPath = htmlDocOut.substring(0, htmlDocOut.lastIndexOf('/'))
@@ -84,6 +84,8 @@ function assembleXml(dom) {
     const DOMParser = dom.window.DOMParser
     const xmlParser = new DOMParser
     var courselists = dom.window.document.querySelectorAll("[data-courses]"); // Find elements that are a course list (defined by having a data-courses attribute referring to the corresponding XML file)
+
+    var ldjsonCourses = "";
     courselists.forEach(courselist => {
     
         var xmlFile = courselist.getAttribute("data-courses");
@@ -91,88 +93,92 @@ function assembleXml(dom) {
         var xmlString = fs.readFileSync("src/data/" + xmlFile + ".xml").toString();
         const xmlDoc = xmlParser.parseFromString(xmlString, "text/xml")
 
-            var template = courselist.firstElementChild;
-            courselist.innerHTML = "";
+        var template = courselist.firstElementChild;
+        courselist.innerHTML = "";
 
-            Array.from(xmlDoc.documentElement.children).forEach(course => {// Cast each course given in the XML document into an HTML representation and append to HTML course list (courselist)
-        
-                var htmlCourse = template.content.cloneNode(true)
-        
-                var illustration = htmlCourse.querySelector("[data-course='illustration']") // Get the element which needs to be filled with content (here, an attribute will have to be set)
-                var data = course.getAttribute("illustration")  // Get the data to fill in from the XML
-                if(illustration) {
-                    if(data) {
-                        illustration.src = "/" + data
-                    } else {
-                        illustration.classList.add('hidden')
-                    }
+        Array.from(xmlDoc.documentElement.children).forEach(course => {// Cast each course given in the XML document into an HTML representation and append to HTML course list (courselist)
+    
+            var htmlCourse = template.content.cloneNode(true)
+            var ldjsonCourse = '{"@context":"http://schema.org","@type":"Course","name":"%title%","description":"%description%","provider":{"@type":"Organization","name":"Pro Students Cottbus","sameAs":"https://pro-students.kiekbjul.de"}},\n'
+    
+            var illustration = htmlCourse.querySelector("[data-course='illustration']") // Get the element which needs to be filled with content (here, an attribute will have to be set)
+            var data = course.getAttribute("illustration")  // Get the data to fill in from the XML
+            if(illustration) {
+                if(data) {
+                    illustration.src = "/" + data
+                } else {
+                    illustration.classList.add('hidden')
                 }
-        
-                var title = htmlCourse.querySelector("[data-course='title']") // Similar to above; here innerhtml content will be set
-                data = course.getElementsByTagName("title")[0].innerHTML
-                if(title) {
-                    if(data) {
-                        title.innerHTML = data
-                    } else {
-                        title.classList.add('hidden')
-                    }
+            }
+    
+            var title = htmlCourse.querySelector("[data-course='title']") // Similar to above; here innerhtml content will be set
+            data = course.getElementsByTagName("title")[0].innerHTML
+            ldjsonCourse = ldjsonCourse.replace('%title%', data)
+            if(title) {
+                if(data) {
+                    title.innerHTML = data
+                } else {
+                    title.classList.add('hidden')
                 }
-        
-                var description = htmlCourse.querySelector("[data-course='description']")
-                data = course.getElementsByTagName("description")[0].innerHTML
-                if(description) {
-                    if(data) {
-                        description.innerHTML = data
-                    } else {
-                        description.classList.add('hidden')
-                    }
+            }
+    
+            var description = htmlCourse.querySelector("[data-course='description']")
+            data = course.getElementsByTagName("description")[0].innerHTML
+            ldjsonCourse = ldjsonCourse.replace('%description%', data)
+            if(description) {
+                if(data) {
+                    description.innerHTML = data
+                } else {
+                    description.classList.add('hidden')
                 }
-        
-                var lessons = htmlCourse.querySelector("[data-course='lessons']")
-                data = course.getElementsByTagName("lessons")[0]
-                if(lessons) {
-                    if(data) {
-                        var lessonTemplate = lessons.firstElementChild // Similar to initial approach with courses: we have a "template" list element that can be reproduced to insert actual content
-                        lessons.innerHTML = ""
+            }
     
-                        Array.from(data.children).forEach(lesson => {
-    
-                            var li = lessonTemplate.cloneNode(true);
-                            li.firstElementChild.innerHTML = lesson.innerHTML
-                            lessons.appendChild(li)
-    
-                        })
-    
-                    } else {
-                        htmlCourse.querySelector("h3.lessons-heading").classList.add('hidden')
-                        lessons.classList.add('hidden')
-                    }
+            var lessons = htmlCourse.querySelector("[data-course='lessons']")
+            data = course.getElementsByTagName("lessons")[0]
+            if(lessons) {
+                if(data) {
+                    var lessonTemplate = lessons.firstElementChild // Similar to initial approach with courses: we have a "template" list element that can be reproduced to insert actual content
+                    lessons.innerHTML = ""
+
+                    Array.from(data.children).forEach(lesson => {
+
+                        var li = lessonTemplate.cloneNode(true);
+                        li.firstElementChild.innerHTML = lesson.innerHTML
+                        lessons.appendChild(li)
+
+                    })
+
+                } else {
+                    htmlCourse.querySelector("h3.lessons-heading").classList.add('hidden')
+                    lessons.classList.add('hidden')
                 }
-        
-                var contents = htmlCourse.querySelector("[data-course='contents']")
-                data = course.getElementsByTagName("contents")[0]
-                if(contents) {
-                    if(data) {
+            }
     
-                        var contentTemplate = contents.firstElementChild
-                        contents.innerHTML = ""
-    
-                        Array.from(data.children).forEach(content => {
-    
-                            var li = contentTemplate.cloneNode(true);
-                            li.firstElementChild.innerHTML = content.innerHTML
-                            contents.appendChild(li)
-    
-                        })
-                    } else {
-                        htmlCourse.querySelector("h3.contents-heading").classList.add('hidden')
-                        contents.classList.add('hidden')
-                    }
+            var contents = htmlCourse.querySelector("[data-course='contents']")
+            data = course.getElementsByTagName("contents")[0]
+            if(contents) {
+                if(data) {
+
+                    var contentTemplate = contents.firstElementChild
+                    contents.innerHTML = ""
+
+                    Array.from(data.children).forEach(content => {
+
+                        var li = contentTemplate.cloneNode(true);
+                        li.firstElementChild.innerHTML = content.innerHTML
+                        contents.appendChild(li)
+
+                    })
+                } else {
+                    htmlCourse.querySelector("h3.contents-heading").classList.add('hidden')
+                    contents.classList.add('hidden')
                 }
-    
-                courselist.appendChild(htmlCourse);
-            })
+            }
+
+            courselist.appendChild(htmlCourse);
+            ldjsonCourses += ldjsonCourse
+        })
     })
 
-    return dom;
+    return ldjsonCourses;
 }
